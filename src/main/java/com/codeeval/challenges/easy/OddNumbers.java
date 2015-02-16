f593a8c0056b32bff73a2917c558cd6f7d66a3ca
java <java@skynet>
2015-02-16 14:40:50 +0100

    Solved OddNumbers

---
+++ src/main/java/com/codeeval/challenges/easy/OddNumbers.java
@@ -0,0 +1,23 @@
+package com.codeeval.challenges.easy;
+
+/**
+ * {@link https://www.codeeval.com/open_challenges/25}
+ * @author Esteban Lopez Valecky
+ */
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		
+		for ( int i = 0; i < 100; i++ ) {
+			
+			if ( i % 2 != 0 ) {
+				
+				System.out.println( i );
+				
+			}
+			
+		}
+
+	}
+
+}
